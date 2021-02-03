ab451194e2d86942c2add11dbdd71cd250cd90f9
geronimomark <mark_geronimo@rocketmail.com>
2021-02-03 23:37:38 +0800

    get data from sample api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-demo';
+  title = 'Angular Demo for Avaloq';
+  springBootData;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:8080/').subscribe(data => {
+        this.springBootData = data;
+        console.log(data);
+    })
+  }
 }
